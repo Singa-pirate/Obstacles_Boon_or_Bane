6cb46799159572c85e9f74b44f81224563a5663e
0-yibai <110611841+0-yibai@users.noreply.github.com>
2023-02-24 16:32:53 +0800

    level transition popup

--- Game&UI/Level_failed.gd
+++ Game&UI/Level_failed.gd
@@ -4,15 +4,11 @@ func _ready():
 	self.position = get_viewport_rect().size / 2
 	$Sprite.scale = Vector2(2,2)
 	$Label.set_position(Vector2(-100,-100))
-	$Home_button.set_position(Vector2(-150,0))
-	$Next_level_button.set_position(Vector2(-60,0))
-	$Replay_level_button.set_position(Vector2(60,0))
+	$Home_button.set_position(Vector2(-100,0))
+	$Replay_level_button.set_position(Vector2(10,0))
 
 func _on_Home_button_pressed():
 	get_parent().go_home_from_popup()
-
-func _on_Next_level_button_pressed():
-	get_parent().next_level_from_popup()
 
 func _on_Replay_level_button_pressed():
 	get_parent().replay_this_level_from_popup()
