a55fb48bc502ecb4bfb8bae51ce94273748003ef
0-yibai <110611841+0-yibai@users.noreply.github.com>
2023-02-24 15:16:38 +0800

    level transition

--- Game&UI/Level_selection.gd
+++ Game&UI/Level_selection.gd
@@ -10,13 +10,17 @@ var UNIT_VERTICAL = 50
 func _ready():
 	$Level1Button.text = "1"
 	$Level2Button.text = "2"
+	$Level3Button.text = "3"
 	
 	$Level1Button.rect_position += Vector2(UNIT_HORIZONTAL, UNIT_VERTICAL)
 	$Level2Button.rect_position += Vector2(UNIT_HORIZONTAL * 2, UNIT_VERTICAL)
-
+	$Level3Button.rect_position += Vector2(UNIT_HORIZONTAL * 3, UNIT_VERTICAL)
 
 func _on_Level1Button_pressed():
 	get_parent().start_level(1)
 
 func _on_Level2Button_pressed():
 	get_parent().start_level(2)
+
+func _on_Level3Button_pressed():
+	get_parent().start_level(3)
