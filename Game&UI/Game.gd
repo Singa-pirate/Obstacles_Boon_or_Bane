e5d0b115467aff1a26be85d0074bc05c34c13eeb
0-yibai <110611841+0-yibai@users.noreply.github.com>
2023-02-24 13:08:28 +0800

    level selection

--- Game&UI/Game.gd
+++ Game&UI/Game.gd
@@ -6,17 +6,26 @@ var levels = {
 	1: preload("res://Levels/Level1.tscn"),
 	2: preload("res://Levels/Level2.tscn")
 }
+var Level_selection = preload("res://Game&UI/Level_selection.tscn")
 
 var current_level = levels[1].instance()
+var level_selection = Level_selection.instance()
 
 func _ready():
-	add_child(current_level)
+	add_child(level_selection)
 
-# Go to the level_number level
+# Go to the next level from previous level
 func next_level():
 	remove_child(current_level)
 	current_level.queue_free()
 	level_number += 1
+	current_level = levels[level_number].instance()
+	add_child(current_level)
+
+# Go to a selected level from level selection scene
+func start_level(level_number):
+	remove_child(level_selection)
+	level_selection.queue_free()
 	current_level = levels[level_number].instance()
 	add_child(current_level)
 
---
+++ Game&UI/Level_selection.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+var UNIT_HORIZONTAL = 50
+var UNIT_VERTICAL = 20
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Level1Button.text = "1"
+	$Level2Button.text = "2"
+	
+	$Level1Button.rect_position += Vector2(UNIT_HORIZONTAL, UNIT_VERTICAL)
+	$Level2Button.rect_position += Vector2(UNIT_HORIZONTAL * 2, UNIT_VERTICAL)
+
+
+func _on_Level1Button_pressed():
+	get_parent().start_level(1)
+
+func _on_Level2Button_pressed():
+	get_parent().start_level(2)
