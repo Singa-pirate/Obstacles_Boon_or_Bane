a4fadaa6c9ba46dabc8ca4a75874202c5d1796c2
0-yibai <110611841+0-yibai@users.noreply.github.com>
2023-02-24 15:48:48 +0800

    home button

---
+++ Elements/Buttons/HomeButton.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_HomeButton_pressed():
+	get_parent().get_parent().go_home()
--- Game&UI/Level_transition.gd
+++ Game&UI/Level_transition.gd
@@ -3,12 +3,12 @@ extends Node2D
 
 
 func _on_Home_button_pressed():
-	get_parent().go_home()
+	get_parent().go_home_from_popup()
 
 
 func _on_Next_level_button_pressed():
-	get_parent().next_level()
+	get_parent().next_level_from_popup()
 
 
 func _on_Replay_level_button_pressed():
-	get_parent().replay_this_level()
+	get_parent().replay_this_level_from_popup()
