5dce99461fe80e20e87ff517efd43a74508046ac
nknguyenhc <nknguyentdn@gmail.com>
2023-04-01 19:16:48 +0800

    Added Tank Selection Esthetics

--- Elements/Buttons/StartButton.gd
+++ Elements/Buttons/StartButton.gd
@@ -6,5 +6,6 @@ func _on_Button_pressed():
 		if child.is_in_group("Meteor"):
 			child.start()
 	get_parent().get_node("Indicator").queue_free()
-	get_parent().get_node("TankSelection").queue_free()
+	if get_parent().get_node("TankSelection") != null:
+		get_parent().get_node("TankSelection").queue_free()
 	queue_free()
