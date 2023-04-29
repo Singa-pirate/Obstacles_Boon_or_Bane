bc8d53a27c146b742e921806feeb8e4aceedb0e6
nknguyenhc <nknguyentdn@gmail.com>
2023-04-29 09:38:03 +0800

    Updated Rigid Obstacle
    Pressing shift -> not changing the charge

--- Obstacles/rigid_obstacle.gd
+++ Obstacles/rigid_obstacle.gd
@@ -14,7 +14,7 @@ func _ready():
 	label = get_node("ChargeLabel")
 
 func _physics_process(delta):
-	if mouse_hovering:
+	if mouse_hovering and not Input.is_action_pressed("ui_shift"):
 		if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_released("ui_scroll_up")) and charge < UPPER_BOUND:
 			charge += 1
 			update_charge()
